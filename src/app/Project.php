4776ae447ecef5f3f9a875756c52b380eb7ea3e9
Georgy PC <georgekagan@gmail.com>
2017-09-04 23:15:47 +0300

    projects: move insert code into model + insert/select by curr user id

--- src/app/Project.php
+++ src/app/Project.php
@@ -10,4 +10,49 @@ class Project extends Model
     {
         return $this->belongsTo('App\Cat');
     }
+
+    public function addProject($data, $user)
+    {
+        $this['user_id'] = $user->uuid;
+
+        // Project supplier info
+        $this['first_name'] = $data['buyerInfo']['firstName'];
+        $this['last_name'] = $data['buyerInfo']['lastName'];
+        $this['email'] = $data['buyerInfo']['contactEmail'];
+        $this['phone'] = $data['buyerInfo']['contactPhone'];
+        $this['country_id'] = $data['buyerInfo']['country'];
+        $this['city'] = $data['buyerInfo']['city'];
+        $this['addr1'] = $data['buyerInfo']['addressLine1'];
+        $this['addr2'] = $data['buyerInfo']['addressLine2'];
+        $this['company'] = $data['buyerInfo']['companyName'];
+        $this['position'] = $data['buyerInfo']['companyPosition'];
+        $this['comp_desc'] = $data['buyerInfo']['whatCompanyDoes'];
+
+        // Project info
+        $this['name'] = $data['projectInfo']['projectName'];
+        $this['cat_id'] = $data['projectInfo']['cat'];
+        $this['desc'] = $data['projectInfo']['basicDesc'];
+        $this['full_desc'] = $data['projectInfo']['fullDesc'];
+        $this['tech_reqs'] = $data['projectInfo']['techReqs'];
+        $this['dev_reqs'] = $data['projectInfo']['developerReqs'];
+        // Get OS requirements
+        $osReqs = [];
+        foreach ($data['projectInfo']['osReqs'] as $os => $isSet) {
+            if ($isSet) { $osReqs[] = $os; }
+        }
+        $this['os_reqs'] = implode(',', $osReqs);
+
+        // Design
+        $this['logo_json'] = json_encode($data['design']['logoSlogan']);
+        $this['design_json'] = json_encode($data['design']['designIdeas']);
+        $this['design_outline'] = $data['design']['designOutline'];
+
+        // Team & milestones
+        $startDate = $data['milestones']['startDate'];
+        $this['start_date'] = $startDate['year'] . '-' . $startDate['month'] . '-' . $startDate['day'];
+        $this['dev_count'] = $data['milestones']['developerCount'];
+        $this['milestones_json'] = json_encode($data['milestones']['arr']);
+
+        $this->save();
+    }
 }
